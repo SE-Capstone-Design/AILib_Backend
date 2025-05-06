f00dddefad440e15eb3654a0e3bf5bbe28e900fb
KimSeoWon <ksw1912@gmail.com>
2025-05-07 01:17:07 +0900

    first commit

---
+++ src/main/java/com/project/ailib/AilibApplication.java
@@ -0,0 +1,13 @@
+package com.project.ailib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AilibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AilibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/ailib/AilibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ailib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AilibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
